dccee386e2de6e690dd6548a936286a74cb3cbde
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2024-12-05 10:38:30 +0100

    feature: footer

---
+++ src/app/[countryCode]/(main)/datenschutz/page.tsx
@@ -0,0 +1,11 @@
+export default function Datenschutz() {
+  return (
+    <>
+      <div className="h-16"></div>
+      <div>
+        <h1>Datenschutz</h1>
+        <p>Datenschutz content</p>
+      </div>
+    </>
+  )
+}
