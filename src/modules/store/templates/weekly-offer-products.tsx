8f96f6da8b4e26fe8c546d7cf28412975d1c299b
JakSchlager <jami.schlager@gmail.com>
2025-01-21 19:54:45 +0100

    fix: fixed bug when theres no weekly offer

--- src/modules/store/templates/weekly-offer-products.tsx
+++ src/modules/store/templates/weekly-offer-products.tsx
@@ -1,18 +1,6 @@
-import { getWeeklyOffer, listProductsWithSort, WeeklyOffer } from "@lib/data/products"
+import { getWeeklyOffer } from "@lib/data/products"
 import { getRegion } from "@lib/data/regions"
 import ProductPreview from "@modules/products/components/product-preview"
-import { Pagination } from "@modules/store/components/pagination"
-import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
-
-const PRODUCT_LIMIT = 12
-
-type PaginatedProductsParams = {
-  limit: number
-  collection_id?: string[]
-  category_id?: string[]
-  id?: string[]
-  order?: string
-}
 
 export default async function WeeklyOfferProducs({
   countryCode,
@@ -30,11 +18,24 @@ export default async function WeeklyOfferProducs({
       response: { weeklyoffers },
     } = await getWeeklyOffer({})
 
+  if (weeklyoffers.length === 0) {
+    return (
+      <>
+        <div className="mb-8 text-2xl-semi">
+          <h1 data-testid="store-page-title">
+            There is no weekly offer available at the moment
+          </h1>
+        </div>
+      </>
+    )
+  }
 
   return (
     <>
       <div className="mb-8 text-2xl-semi">
-        <h1 data-testid="store-page-title">Weekly Offer {weeklyoffers[0].title}</h1>
+        <h1 data-testid="store-page-title">
+          Weekly Offer {weeklyoffers[0].title}
+        </h1>
       </div>
       <ul
         className="grid grid-cols-2 w-full small:grid-cols-3 medium:grid-cols-4 gap-x-6 gap-y-8"
