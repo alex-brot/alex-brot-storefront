48db1f1b44ffd0d1239f2274476b553dd33050b8
mStenz <michael.stenz2006@gmail.com>
2025-02-05 19:33:43 +0100

    [dev] update image fetch policies

--- next.config.js
+++ next.config.js
@@ -15,19 +15,7 @@ const nextConfig = withStoreConfig({
       },
       {
         protocol: "https",
-        hostname: "media1.giphy.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
+        hostname: "alex-brot.s3.eu-central-1.amazonaws.com",
       },
     ],
   },
