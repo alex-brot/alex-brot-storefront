8c3d21eb7d15fe5c8a8c09d5d453fb75512ed119
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2025-02-06 11:04:23 +0100

    feat: added jakob to team list

--- src/modules/team/static/persons.ts
+++ src/modules/team/static/persons.ts
@@ -3,13 +3,13 @@ import { Person } from "types/person"
 
 export const devs: Person[] = [
   {
-    name: "Michael",
+    name: "Michael S",
     description:
       "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.",
     image: "/public-assets/blank-profile-picture.png",
   },
   {
-    name: "Jakob",
+    name: "Jakob S",
     description:
       "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.",
     image: "/public-assets/blank-profile-picture.png",
@@ -27,7 +27,13 @@ export const devs: Person[] = [
     image: "/public-assets/blank-profile-picture.png",
   },
   {
-    name: "Michael",
+    name: "Michael R",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.",
+    image: "/public-assets/blank-profile-picture.png",
+  },
+  {
+    name: "Jakob H",
     description:
       "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.",
     image: "/public-assets/blank-profile-picture.png",
