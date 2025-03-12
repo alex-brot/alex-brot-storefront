34fc4d6fdefe2dd07820174d6f4a78b4e8017675
lukmayr <l.mayrhuber@students.htl-leonding.ac.at>
2025-03-12 17:02:57 +0100

    feat(checkout): added automatic billing address, gathered address out of shipping method (hopefully nothing breaks xD)

--- src/modules/cart/templates/summary.tsx
+++ src/modules/cart/templates/summary.tsx
@@ -14,10 +14,10 @@ type SummaryProps = {
   }
 }
 
+//TODO: change step order (done)
+
 function getCheckoutStep(cart: HttpTypes.StoreCart) {
-  if (!cart?.shipping_address?.address_1 || !cart.email) {
-    return "address"
-  } else if (cart?.shipping_methods?.length === 0) {
+  if (!cart?.shipping_address?.address_1 || !cart.email || cart?.shipping_methods?.length === 0) {
     return "delivery"
   } else {
     return "payment"
